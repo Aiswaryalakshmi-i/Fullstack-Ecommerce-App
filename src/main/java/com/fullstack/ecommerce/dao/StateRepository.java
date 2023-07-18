1cbc5437e4d7e014904542f05c654998af0ec281
Aiswarya <aishwaryas120@gmail.com>
2023-07-18 11:56:05 +0530

    This is backend

---
+++ src/main/java/com/fullstack/ecommerce/dao/StateRepository.java
@@ -0,0 +1,16 @@
+package com.fullstack.ecommerce.dao;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.query.Param;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.web.bind.annotation.CrossOrigin;
+
+import com.fullstack.ecommerce.entity.State;
+
+@RepositoryRestResource(collectionResourceRel = "states", path = "states")
+public interface StateRepository extends JpaRepository<State, Integer>{
+
+	List<State> findByCountryCode(@Param("code") String code);
+}
